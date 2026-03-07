a1aec49d3438686f2fd1917384128aba0766eb9a
Alexey Tyapkin <Marveleks@gmail.com>
2026-03-08 00:33:23 +0300

    migration: added missed port migration

---
+++ src/Heimdallr.Infrastructure/Database/Migrations/20260307213239_Added_Nullability_On_Port.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Heimdallr.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Nullability_On_Port : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Port",
+                schema: "heimdallr",
+                table: "meter_endpoints",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Port",
+                schema: "heimdallr",
+                table: "meter_endpoints",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
