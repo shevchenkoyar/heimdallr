d1de781c2b9aa87482cbea34c6c671b89802018d
Alexey Tyapkin <Marveleks@gmail.com>
2026-03-30 18:19:24 +0300

    feat: added redirection if not authorized

--- src/Heimdallr.WebUI/Components/Pages/Home.razor.cs
+++ src/Heimdallr.WebUI/Components/Pages/Home.razor.cs
@@ -1,6 +1,5 @@
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Heimdallr.WebUI.Components.Pages;
 
@@ -8,20 +7,4 @@ namespace Heimdallr.WebUI.Components.Pages;
 public partial class Home : ComponentBase
 {
     [Inject] public required NavigationManager NavigationManager { get; set; }
-
-    [Inject] public required AuthenticationStateProvider AuthStateProvider { get; set; }
-
-    private AuthenticationState AuthenticationState { get; set; }
-
-    protected override async Task OnInitializedAsync()
-    {
-        AuthenticationState = await AuthStateProvider.GetAuthenticationStateAsync();
-
-        if (AuthenticationState.User == null || AuthenticationState.User.Identity == null ||
-            !AuthenticationState.User.Identity.IsAuthenticated)
-        {
-            NavigationManager.NavigateTo("/auth");
-        }
-    }
-    
 }
